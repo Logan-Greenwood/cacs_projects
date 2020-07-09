efc18711288f3a794d47a3cd8c9a802c1aa28b6c
Logan-Greenwood <63980928+Logan-Greenwood@users.noreply.github.com>
2020-07-08 22:55:15 -0500

    created physics

--- workbook1.py
+++ workbook1.py
@@ -1,32 +1,9 @@
-lovely_loveseat_description = '''Lovely Loveseat. Tufted polyester blend on wood. 
-32 inches high x 40 inches wide x 30 inches deep. 
-Red or white.'''
-lovely_loveseat_price = 254.00
+def repeat_stuff(stuff, num_repeats=10):
+    return stuff * num_repeats
 
-stylish_settee_description = '''Stylish Settee. Faux leather on birch. 
-29.50 inches high x 54.75 inches wide x 28 inches deep. 
-Black.'''
-stylish_settee_price = 100.50
 
-luxurious_lamp_description = '''Luxurious Lamp. Glass and iron. 
-36 inches tall. 
-Brown with cream shade.'''
-luxurious_lamp_price = 52.15
+lyrics = repeat_stuff("Row ", 3) + "Your Boat.\n"
 
-sales_tax = .088
+song = repeat_stuff(lyrics)
 
-customer_one_total = 0
-customer_one_itemization = ""
-customer_one_total += lovely_loveseat_price
-customer_one_itemization += lovely_loveseat_description
-
-customer_one_total += luxurious_lamp_price
-customer_one_itemization += "\n\n" + luxurious_lamp_description
-
-customer_one_tax = sales_tax * customer_one_total
-customer_one_total += customer_one_tax
-
-print("Customer One Items: \n" +
-      customer_one_itemization)
-
-print(f"Customer One Total: ${customer_one_total:.2f}")
+print(song)
