e3406693a9105445ae61228539d642d46044c704
Logan-Greenwood <63980928+Logan-Greenwood@users.noreply.github.com>
2020-07-09 10:52:36 -0500

    name test

--- workbook1.py
+++ workbook1.py
@@ -1,9 +1,1 @@
-def repeat_stuff(stuff, num_repeats=10):
-    return stuff * num_repeats
-
-
-lyrics = repeat_stuff("Row ", 3) + "Your Boat.\n"
-
-song = repeat_stuff(lyrics)
-
-print(song)
+# name test
