ae16a66ee38d64f72a61660f864be6ef4c53b0f1
Logan-Greenwood <63980928+Logan-Greenwood@users.noreply.github.com>
2020-07-20 13:47:40 -0400

    pizza project

--- workbook1.py
+++ workbook1.py
@@ -1,9 +1,2 @@
-first_names = ['Ainsley', 'Ben', 'Chani', 'Depak']
-age = []
-age.append(42)
-all_ages = [32, 41, 29]
-all_ages += age
-
-name_and_age = zip(first_names, all_ages)
-
-ids = range(len(first_names))
+mylist = ['a', 'b', 'c', 'd', 'e']
+print(mylist[-1])
