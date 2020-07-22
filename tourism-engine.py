d609c2ddd59974797e702f7f46e405ba8fc2f975
Logan-Greenwood <63980928+Logan-Greenwood@users.noreply.github.com>
2020-07-21 20:38:04 -0400

    2/3 boredless tourist

--- tourism-engine.py
+++ tourism-engine.py
@@ -22,4 +22,44 @@ test_destination_index = get_traveler_location(test_traveler)
 
 # begin attractions
 attractions = [[] for i in destinations]
-print(attractions)
+
+
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions_for_destination = attractions[destination_index].append(attraction)
+        return
+    except ValueError:
+        return
+
+
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+
+    for attraction in attractions_in_city:
+        possible_attraction = attraction
+        attraction_tags = attraction[1]
+        for i in interests:
+            if i in attraction_tags:
+                attractions_with_interest.append(possible_attraction)
+    return attractions_with_interest
+
+
+la_arts = find_attractions("Los Angeles, USA", ["art"])
+print(la_arts)
