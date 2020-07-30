6f7139891b2baefe02c77cb025e726a97ba02ac9
Logan-Greenwood <63980928+Logan-Greenwood@users.noreply.github.com>
2020-07-30 15:52:59 -0400

    dictionary project

--- workbook1.py
+++ workbook1.py
@@ -1,7 +1,4 @@
-test = "testing testing 123, 123"
+raffle = {223842: "Teddy Bear", 872921: "Concert Tickets", 320291: "Gift Basket", 412123: "Necklace", 298787: "Pasta Maker"}
 
-for word in test:
-    i = 0
-    if word == "testing":
-        i += 1
-        print(i)
+raffle.pop(561721, "No Value")
+print(raffle)
